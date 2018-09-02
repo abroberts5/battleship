610bebaaa60f4aeaf63205e9cd30a80b24a95b56
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2018-09-02 16:34:16 -0600

    1st assertion

--- test/battleship_test.rb
+++ test/battleship_test.rb
@@ -3,5 +3,10 @@ require 'minitest/pride'
 require './lib/battleship'
 
 class BattleshipTest < Minitest::Test
+  def test_battleship_class_exists
+    battleship = Battleship.new
+
+    assert_instance_of Battleship, battleship
+  end
 
 end
