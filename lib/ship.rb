0dfc1bccc2e9866b5028c063cf2e26527e222344
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2018-09-05 21:57:02 -0600

    Possible WIP

--- lib/ship.rb
+++ lib/ship.rb
@@ -13,11 +13,11 @@ class Ship
   	ship_array << ["C1", "C2", "C3", "C4"]
   	ship_array << ["D1", "D2", "D3", "D4"]
 
-  	@ship_1 << ship_array.sample(1)
+  	@ship_1 << ship_array.sample(2)
 
-  	@ship_2 << ship_array.sample(1)
+  	@ship_2 << ship_array.sample(3)
   		if ship_2 == ship_1
-  			ship_2.replace(ship_arrays.sample(1))
+  			ship_2.replace(ship_arrays.sample(2))
   		end
     end
   end
